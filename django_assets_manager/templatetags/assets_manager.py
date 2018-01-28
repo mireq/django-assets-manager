df91730b1f8693c3b97d4260099084e44586fb48
Miroslav Bendík <miroslav.bendik@gmail.com>
2018-01-28 18:39:07 +0100

    Support for jinja2 context

--- django_assets_manager/templatetags/assets_manager.py
+++ django_assets_manager/templatetags/assets_manager.py
@@ -110,7 +110,11 @@ def get_simple_asset_sources(asset, asset_type):
 
 def get_or_create_unused_assets(context, asset_type):
 	if not "request" in context:
-		context["request"] = FakeRequest()
+		extra = {'request': FakeRequest()}
+		if hasattr(context, 'update'):
+			context.update(extra)
+		else:
+			context.vars.update(extra)
 	if hasattr(context["request"], "unused_" + asset_type):
 		return getattr(context["request"], "unused_" + asset_type)
 	else:
