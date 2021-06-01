8da4a600589cbd4888dc885b056b83fdc2242ed7
Miroslav Bendík <miroslav.bendik@gmail.com>
2021-06-01 12:31:31 +0200

    Django 3.2 don't require default_app_config

--- django_assets_manager/__init__.py
+++ django_assets_manager/__init__.py
@@ -1,1 +1,4 @@
-default_app_config = 'django_assets_manager.apps.AssetsManagerConfig'
+import django
+
+if not django.VERSION >= (3, 2):
+	default_app_config = 'django_assets_manager.apps.AssetsManagerConfig'
